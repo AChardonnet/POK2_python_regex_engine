475414ef6888129cb46d08d5ec31722ef38213e8
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2026-01-27 23:47:32 +0100

    added backtracking of concatenation nodes

--- pyregex/backtracking.py
+++ pyregex/backtracking.py
@@ -1,17 +1,29 @@
-def regex_match_backtrack(tree, text):
-    for index in match_backtrack(tree, text, 0):
+def regex_match_backtrack(node, text):
+    for index in match_backtrack_alternation(node, text, 0):
+        print(index)
         if index == len(text):
             return True
     return False
 
 
-def match_backtrack(tree, text, index):
-    if tree is None:
+def match_backtrack_alternation(node, text, index):
+    if node is None:
         yield index
-    elif isinstance(tree, str):
-        assert len(tree) == 1, "Invalid Tree"
-        if index < len(text) and text[index] == tree:
-            yield index + 1
+    elif isinstance(node, str):
+        assert len(node) == 1, "expected a single character"
+        if index < len(text) and text[index] == node:
+            yield 1 + index
+    elif node[0] == "concatenation":
+        yield from match_backtrack_concatenation(node, text, index)
 
 
-print(regex_match_backtrack("a", "a"))
+def match_backtrack_concatenation(node, text, index):
+    met = set()
+    for index1 in match_backtrack_alternation(node[1], text, index):
+        if index1 in met:
+            continue
+        met.add(index1)
+        yield from match_backtrack_alternation(node[2], text, index1)
+
+
+print(regex_match_backtrack(("concatenation", "a", "b"), "ab"))
--- tests/test_backtracking.py
+++ tests/test_backtracking.py
@@ -8,3 +8,14 @@ def test_backtracking_None():
 def test_backtracking_single_char():
     assert backtracking.regex_match_backtrack("a", "a") == True
     assert backtracking.regex_match_backtrack("a", "b") == False
+
+
+def test_backtracking_concatenation():
+    assert backtracking.regex_match_backtrack(("concatenation", "a", "b"), "ab") == True
+    assert backtracking.regex_match_backtrack(("concatenation", "a", "b"), "a") == False
+    assert (
+        backtracking.regex_match_backtrack(("concatenation", "a", "b"), "ba") == False
+    )
+    assert (
+        backtracking.regex_match_backtrack(("concatenation", "a", "b"), "aab") == False
+    )
